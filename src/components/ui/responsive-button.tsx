3adcccf7d80da0e8dc34c30c7ff16bd7ae829b53
Alessandro <a@a.a>
2024-09-16 11:07:16 +0200

    update

--- src/components/ui/responsive-button.tsx
+++ src/components/ui/responsive-button.tsx
@@ -1,22 +1,23 @@
-import { type ButtonProps, buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { Slot } from "@radix-ui/react-slot";
-import { type LucideProps } from "lucide-react";
-import React from "react";
+import { type ButtonProps, buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import type { IconProps } from '@radix-ui/react-icons/dist/types'
+import { Slot } from '@radix-ui/react-slot'
+import { type LucideProps } from 'lucide-react'
+import React from 'react'
 
 export interface ResponsiveButtonProps extends ButtonProps {
-  label: string;
-  icon: React.ComponentType<LucideProps>;
-  iconProps?: LucideProps;
-  alwaysShowLabel?: boolean;
+  label: string
+  icon: React.ComponentType<IconProps> | React.ComponentType<LucideProps>
+  iconProps?: IconProps
+  alwaysShowLabel?: boolean
 }
 
 const icon_sizes = {
-  default: "h-5 w-5",
-  sm: "h-3.5 w-3.5",
-  lg: "h-6 w-6",
-  icon: "h-9 w-9",
-};
+  default: 'h-5 w-5',
+  sm: 'h-3.5 w-3.5',
+  lg: 'h-6 w-6',
+  icon: 'h-9 w-9',
+}
 
 export const ResponsiveButton = React.forwardRef<
   HTMLButtonElement,
@@ -37,28 +38,28 @@ export const ResponsiveButton = React.forwardRef<
     },
     ref,
   ) => {
-    const icon_size = size ? icon_sizes[size] : icon_sizes["default"];
+    const icon_size = size ? icon_sizes[size] : icon_sizes['default']
 
-    const Comp = asChild ? Slot : "button";
+    const Comp = asChild ? Slot : 'button'
     return (
       <Comp
-        className={cn("gap-2", buttonVariants({ variant, size, className }))}
+        className={cn('gap-2', buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       >
         <IconComponent className={icon_size} {...iconProps} />
         <span
           className={cn(
-            alwaysShowLabel ? "" : "sr-only sm:not-sr-only",
-            "sm:whitespace-nowrap",
+            alwaysShowLabel ? '' : 'sr-only sm:not-sr-only',
+            'sm:whitespace-nowrap',
           )}
         >
           {label}
         </span>
         {children}
       </Comp>
-    );
+    )
   },
-);
+)
 
-ResponsiveButton.displayName = "ResponsiveButton";
+ResponsiveButton.displayName = 'ResponsiveButton'
