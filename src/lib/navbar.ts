26fa15c09318ba4f346c9546b9aed2faad0ef838
Alessandro <a@a.a>
2024-09-26 18:43:40 +0200

    update index and add masterclass link

--- src/lib/navbar.ts
+++ src/lib/navbar.ts
@@ -3,9 +3,10 @@ import type { LucideProps } from "lucide-react"
 export interface NavBarItem {
     label: string,
     href: string,
+    className?: string,
     icon?: React.ForwardRefExoticComponent<LucideProps>
 }
 
 export interface NavBarItemsProps {
     items: NavBarItem[]
-  }
+}
